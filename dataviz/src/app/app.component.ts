3ec73afc2255601c456f36adb3111bbc5489028a
Ryan Williamson <ryanthomas1998@gmail.com>
2019-03-18 20:08:49 -0400

    Added heatmap functionality

--- dataviz/src/app/app.component.ts
+++ dataviz/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { DataService } from './data.service';
+import 'leaflet.heat';
+
 
 declare let L;
 
@@ -18,25 +20,37 @@ export class AppComponent {
 
   ngOnInit() {
     const map = L.map('map').setView([40.0142, -83.0309], 13);
-    var CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+    let CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       subdomains: 'abcd',
       maxZoom: 19
     });
-    var CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+    let CartoDB_DarkMatter = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       subdomains: 'abcd',
       maxZoom: 19
     });
-    var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    let CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       subdomains: 'abcd',
       maxZoom: 19
     });
 
     CartoDB_Voyager.addTo(map);
+
     this.dataService.getTwitter().subscribe(data => {
       // L.geoJson(data).addTo(map);
+      let features = [];
+      let coords = [];
+      L.geoJson(data, {
+        onEachFeature: (feature, layer) => {
+          features.push(feature);
+          let latlng = new L.latLng(feature.geometry.coordinates[1],feature.geometry.coordinates[0])
+          coords.push(latlng)
+        }
+      });
+      console.log(coords);
+      var heat = L.heatLayer(coords).addTo(map);
     });
 
 
