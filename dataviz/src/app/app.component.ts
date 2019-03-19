c7f60a7100b1829858fe9dae750a4cd4c95260e4
Ryan Williamson <ryanthomas1998@gmail.com>
2019-03-18 23:48:40 -0400

    Added a heatmap and time slider functionality

--- dataviz/src/app/app.component.ts
+++ dataviz/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { DataService } from './data.service';
 import 'leaflet.heat';
-
+import * as turf from '@turf/turf'
+import 'hammerjs';
 
 declare let L;
 
@@ -14,12 +15,16 @@ declare let L;
 
 export class AppComponent {
 
+  time = 0;
+  heatmap;
+  map;
+  features = [];
   constructor(private dataService: DataService) {
 
   }
 
   ngOnInit() {
-    const map = L.map('map').setView([40.0142, -83.0309], 13);
+    this.map = L.map('map').setView([40.0142, -83.0309], 13);
     let CartoDB_Positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       subdomains: 'abcd',
@@ -36,23 +41,36 @@ export class AppComponent {
       maxZoom: 19
     });
 
-    CartoDB_Voyager.addTo(map);
+    CartoDB_Voyager.addTo(this.map);
+
 
     this.dataService.getTwitter().subscribe(data => {
       // L.geoJson(data).addTo(map);
-      let features = [];
+
       let coords = [];
       L.geoJson(data, {
         onEachFeature: (feature, layer) => {
-          features.push(feature);
-          let latlng = new L.latLng(feature.geometry.coordinates[1],feature.geometry.coordinates[0])
+          this.features.push(feature);
+          let latlng = new L.latLng(feature.geometry.coordinates[1], feature.geometry.coordinates[0])
           coords.push(latlng)
         }
       });
       console.log(coords);
-      var heat = L.heatLayer(coords).addTo(map);
+      this.heatmap = L.heatLayer(coords).addTo(this.map);
     });
 
+
+  }
+
+  filterHeatmap() {
+    let points = this.features.filter(f => {
+      let date = new Date(f.properties.time);
+      return date.getHours() == this.time;
+    }).map(f => {
+      return new L.latLng(f.geometry.coordinates[1], f.geometry.coordinates[0])
+    });
+    this.map.removeLayer(this.heatmap);
+    this.heatmap = L.heatLayer(points, {radius: 25, maxZoom:18}).addTo(this.map);
 
   }
 }
--- dataviz/src/app/app.module.ts
+++ dataviz/src/app/app.module.ts
@@ -4,7 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
-
+import {MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button'
+import {MatSliderModule} from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,11 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSliderModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
